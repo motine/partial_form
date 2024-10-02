face356bcbc25027eed2fa01f3d732266a2c34f9
Tom Rothe <info@tomrothe.de>
2024-10-02 12:47:51 +0200

    change to minitest, adapt development dependencies, tests for generator

--- Gemfile
+++ Gemfile
@@ -3,5 +3,6 @@ source "https://rubygems.org"
 gemspec
 
 gem "rake", "~> 13.0"
-gem "test-unit", "~> 3.0"
 gem "standard", "~> 1.3"
+gem "minitest"
+gem "railties", ">= 7.1"
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,6 @@
-# frozen_string_literal: true
+require "bundler/setup"
+require "minitest/autorun"
+require "minitest/pride"
 
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "partial_form"
-
-require "test-unit"
